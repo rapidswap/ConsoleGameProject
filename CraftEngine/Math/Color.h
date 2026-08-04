8976ee339b88d016aeedda2c0bcac01b3f2d2944
rapidswap <curudow7988@gmail.com>
2026-08-04 12:04:42 +0900

    Vector and Color

---
+++ CraftEngine/Math/Color.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <Core/Core.h>
+#include <Windows.h>
+
+namespace Carft
+{
+	enum class CRAFT_API Color : WORD
+	{
+		Red=FOREGROUND_RED,
+		Green=FOREGROUND_GREEN,
+		Blue=FOREGROUND_BLUE,
+		Yellow = Red | Green,
+		Gyan = Green | Blue,
+		Purple = Red | Blue,
+		White = Red | Green | Blue,
+		BrightWhite = White | FOREGROUND_INTENSITY
+
+
+
+
+	};
+}
---
+++ CraftEngine/Math/Vector2.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <Core/Core.h>
+#include <Windows.h>
+
+namespace Craft
+{
+	class CRAFT_API Vector2
+	{
+	public:
+		Vector2(int x = 0, int y = 0);
+		~Vector2() = default;
+
+		// 연산자 오버로딩.
+
+		// Windows 콘솔 좌표계로 변환하는 연산자 오버로딩.
+		operator COORD ();
+		operator COORD () const;
+
+		// 사칙 연산자 오버로딩.
+		Vector2 operator+(const Vector2& other) const;
+		Vector2 operator-(const Vector2& other) const;
+		Vector2 operator*(const Vector2& other) const;
+		Vector2 operator/(const Vector2& other) const;
+
+		// 대입 연산자 오버로딩.
+		Vc
+
+		// 비교 연산자 오버로딩.
+
+		// 자주 사용할 값을 전역 변수로 선언.
+		static Vector2 Zero;
+		static Vector2 One;
+		static Vector2 Right;
+		static Vector2 Up;
+	public:
+
+		// 좌표계 값으로 사용하기 위한 변수.
+		int x = 0;
+		int y = 0;
+	};
+}
