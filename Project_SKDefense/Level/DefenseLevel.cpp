4b0b89cd8ef1d7c7d55cbcbedd7d1d7ff3eb09cb
Hyeongjin Kang <73596570+rapidswap@users.noreply.github.com>
2026-08-21 17:46:36 +0900

    fix: rename Update to Tick and use Level's cameraPosition

--- Project_SKDefense/Level/DefenseLevel.cpp
+++ Project_SKDefense/Level/DefenseLevel.cpp
@@ -4,9 +4,10 @@
 #include <fstream>
 #include <iostream>
 
+using namespace Craft;
 DefenseLevel::DefenseLevel()
 {
-	cameraOffset = Craft::Vector2(0, 0);
+	cameraPosition = Craft::Vector2(0, 0);
 	LoadMap();
 }
 
@@ -24,28 +25,28 @@ void DefenseLevel::LoadMap()
 	}
 }
 
-void DefenseLevel::Update(float deltaTime)
+void DefenseLevel::Tick(float deltaTime)
 {
 	// WASD 카메라 이동 로직
 	// GetKey는 누르고 있는 동안 계속 true이므로 부드러운 스크롤은 안 될 수도 있지만, 
 	// GetKeyDown과 GetKey 조합 혹은 프레임 단위 이동으로 처리합니다.
-	if (Craft::Input::Get().GetKey('W')) cameraOffset.y -= 1;
-	if (Craft::Input::Get().GetKey('S')) cameraOffset.y += 1;
-	if (Craft::Input::Get().GetKey('A')) cameraOffset.x -= 1;
-	if (Craft::Input::Get().GetKey('D')) cameraOffset.x += 1;
+	if (Craft::Input::Get().GetKey('W')) cameraPosition.y -= 1;
+	if (Craft::Input::Get().GetKey('S')) cameraPosition.y += 1;
+	if (Craft::Input::Get().GetKey('A')) cameraPosition.x -= 1;
+	if (Craft::Input::Get().GetKey('D')) cameraPosition.x += 1;
 
-	// 카메라 오프셋이 음수가 되지 않도록 제한 (옵션)
-	if (cameraOffset.x < 0) cameraOffset.x = 0;
-	if (cameraOffset.y < 0) cameraOffset.y = 0;
+	// 카메라 좌표가 음수가 되지 않도록 제한 (옵션)
+	if (cameraPosition.x < 0) cameraPosition.x = 0;
+	if (cameraPosition.y < 0) cameraPosition.y = 0;
 	
-	// 최대 카메라 오프셋 제한 (맵 사이즈 기반)
+	// 최대 카메라 좌표 제한 (맵 사이즈 기반)
 	int maxCamX = (mapData.empty() ? 0 : mapData[0].size()) - 30; // 화면 가로 크기 대략 30으로 가정
 	int maxCamY = mapData.size() - 20; // 화면 세로 크기 대략 20으로 가정
 	if (maxCamX < 0) maxCamX = 0;
 	if (maxCamY < 0) maxCamY = 0;
 
-	if (cameraOffset.x > maxCamX) cameraOffset.x = maxCamX;
-	if (cameraOffset.y > maxCamY) cameraOffset.y = maxCamY;
+	if (cameraPosition.x > maxCamX) cameraPosition.x = maxCamX;
+	if (cameraPosition.y > maxCamY) cameraPosition.y = maxCamY;
 }
 
 void DefenseLevel::Draw()
@@ -56,7 +57,7 @@ void DefenseLevel::Draw()
 		for (int x = 0; x < (int)mapData[y].size(); ++x)
 		{
 			// 화면 좌표로 변환
-			Craft::Vector2 screenPos(x - cameraOffset.x, y - cameraOffset.y);
+			Craft::Vector2 screenPos(x - cameraPosition.x, y - cameraPosition.y);
 
 			char tile = mapData[y][x];
 			std::string tileStr(1, tile);
--- Project_SKDefense/Level/DefenseLevel.h
+++ Project_SKDefense/Level/DefenseLevel.h
@@ -11,7 +11,7 @@ public:
 	DefenseLevel();
 	~DefenseLevel() = default;
 
-	virtual void Update(float deltaTime) override;
+	virtual void Tick(float deltaTime) override;
 	virtual void Draw() override;
 
 private:
@@ -21,7 +21,4 @@ private:
 private:
 	// 맵 데이터
 	std::vector<std::string> mapData;
-	
-	// 카메라 오프셋
-	Craft::Vector2 cameraOffset;
 };
